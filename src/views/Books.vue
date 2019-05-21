179c0ad62c525e0c5f1b48584fec184bf410745a
KidsXH <wlzeign@hotmail.com>
2019-05-21 15:44:28 +0800

    update books view and recommendations view

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,192 +1,94 @@
 <template>
-  <div>
-    <br/>
-    <div>
-      <v-layout align="left">
-              <span v-for="n in 10"
-                    :key="n">
-                &nbsp;
-              </span>
-        <span style="font-size: 18px;">temp0: &nbsp; &nbsp;</span>
-        <v-chip style="margin-top: 0px" outline color="green"
-                @click="addChip"
-                v-for="item in classification"
-                :key="item"
-        >
-          {{item}}
-        </v-chip>
-
-      </v-layout>
-    </div>
-    <br/>
-        <div>
-          <v-layout align="left">
-              <span v-for="n in 10"
-                    :key="n">
-                &nbsp;
-              </span>
-              <span style="font-size: 18px;">temp1: &nbsp; &nbsp;</span>
-              <v-chip style="margin-top: 0px" outline color="primary"
-                      @click="addChip"
-                      v-for="word in wordNum"
-                      :key="word"
-              >
-                {{word}}
-              </v-chip>
-
-          </v-layout>
-        </div>
-        <br/>
-        <div>
-          <v-layout row wrap align="left">
-              <span v-for="n in 10"
-                    :key="n">
-                &nbsp;
-              </span>
-            <span style="font-size: 18px;">temp2: &nbsp; &nbsp;</span>
-            <v-chip style="margin-top: 0px" outline color="red"
-                    @click="addChip"
-                    v-for="stock in inStock"
-                    :key="stock">
-              {{stock}}
-            </v-chip>
-          </v-layout>
-        </div>
-        <br/>
-        <div>
-          <v-layout row wrap>
-            <span v-for="n in 10"
-                  :key="n">
-                &nbsp;
-            </span>
-            <span style="font-size: 10px;">
-              已选择的标签：
-            </span>
-
-            <span style="font-size: 10px;"
-                  v-if="curChips.length==0"
-            >
-              暂无选择的标签
-            </span>
-            <v-chip style="margin-top: -5px"
-                    close
-                    v-for = "chip in curChips"
-                    v-model = "chip.yes"
-                    :key="chip.inText"
-            >
-              {{chip.inText}}
-
-            </v-chip>
-
-          </v-layout>
-          <v-select
-                  style="margin: -40px 30px -34px 1600px;"
-                  :items="sortItems"
-                  label="排序方式"
-                  solo
-          >
-          </v-select>
-        </div>
-        <br/>
-        <v-card flat>
-          <v-container fluid grid-list-md>
-            <v-layout wrap row>
-              <v-flex
-                      v-for="n in 6"
-                      :key="n"
-                      style="margin-bottom: 25px"
-              >
-                <BookCard></BookCard>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-    <br/>
-    <div class="text-xs-right">
-      <v-pagination
-
-              v-model="page"
-              :length="4"
-      ></v-pagination>
-      <br/>
-      <span> &nbsp;</span>
-    </div>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <selection-card/>
+      </v-flex>
+      <v-flex xs12>
+        <div class="separate-line"></div>
+      </v-flex>
+      <v-flex xs12 style="margin-top: -1rem">
+        <book-grid-list title="List" pagination/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import BookCard from "../components/BookCard";
+  import SelectionCard from "../components/SelectionCard";
+  import BookGridList from "../components/BookGridList";
+
   export default {
-    components: {BookCard},
+    components: {BookGridList, SelectionCard},
     props: ['parentToChild'],
-    data () {
+    data() {
       return {
         active: 0,//当前分类页码
         page: 1,//当前分页页码
         isSame: false,
-        classification: ["item1","item2","item3","item4","item5"],
-        sortItems: ["按点击量排列","按星级排列"],
-        wordNum:["0-100","100-1000","1000-1w"],
-        inStock:["有","无"],
-        newChip:{//新筛选框
+        classification: ["item1", "item2", "item3", "item4", "item5"],
+        sortItems: ["按点击量排列", "按星级排列"],
+        wordNum: ["0-100", "100-1000", "1000-1w"],
+        inStock: ["有", "无"],
+        newChip: {//新筛选框
           inText: "",
-          yes:true,
+          yes: true,
         },
-        curChips:[],//当前存在的筛选框
+        curChips: [],//当前存在的筛选框
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
       }
     },
     methods: {
-      test:function(text){
+      test: function (text) {
         alert(text);
       },
-      add: function(fromParentText){
+      add: function (fromParentText) {
         alert(fromParentText);
-        this.newChip={
+        this.newChip = {
           inText: fromParentText,
-          yes:true,
+          yes: true,
         };
         this.curChips.push(this.newChip);
       },
-      addChip: function(e) {
-        this.newChip={
+      addChip: function (e) {
+        this.newChip = {
           inText: e.target.innerText,
-          type:"",
-          yes:true,
+          type: "",
+          yes: true,
         };
-        for(let i = 0;i<this.curChips.length;i++){
-          if(this.curChips[i].inText == e.target.innerText){
+        for (let i = 0; i < this.curChips.length; i++) {
+          if (this.curChips[i].inText == e.target.innerText) {
             this.isSame = true;
             this.curChips[i].yes = true;
           }
         }
-        if(!this.isSame){
+        if (!this.isSame) {
           this.curChips.push(this.newChip);
         }
         this.isSame = false;
-        this.newChip={
+        this.newChip = {
           inText: "",
           type: "",
-          yes:true,
+          yes: true,
         };
       },
-      addChip_search: function(text){
-        this.newChip={
-          inText: "搜索结果:"+text,
-          type:"",
-          yes:true,
+      addChip_search: function (text) {
+        this.newChip = {
+          inText: "搜索结果:" + text,
+          type: "",
+          yes: true,
         };
-        if(!this.isSame){
+        if (!this.isSame) {
           this.curChips.push(this.newChip);
         }
         this.isSame = false;
-        this.newChip={
+        this.newChip = {
           inText: "",
           type: "",
-          yes:true,
+          yes: true,
         };
       },
-      changePage:function () {
+      changePage: function () {
         alert(this.page);
       }
     }
--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -3,24 +3,24 @@
     <v-container>
       <v-layout row wrap>
         <v-flex xs12>
-          <recommend-card v-bind="post"></recommend-card>
+          <book-grid-list :title="$t('recommendations')" :cards="post.cards"/>
         </v-flex>
         <v-flex xs12>
           <div class="separate-line"></div>
         </v-flex>
         <v-flex xs12>
-          <recommend-card v-bind="post"></recommend-card>
+          <book-grid-list :title="$t('hello')" :cards="post.cards"/>
         </v-flex>
       </v-layout>
     </v-container>
   </div>
 </template>
 <script>
-  import RecommendCard from "../components/RecommendationCard";
+  import BookGridList from "../components/BookGridList";
 
   export default {
     name: "recommendations",
-    components: {RecommendCard},
+    components: {BookGridList},
     data: () => ({
       post: {
         cards: [
@@ -28,17 +28,16 @@
             points: 20,   //借阅所需积分
             hot: 8888.66,   //热度
             can_borrowed: true,  //是否可以借书
-            isreviewd: false, //是否已经评星
             tag1: '0~1000字',   //标签1
             tag2: '网络',   //标签2
             tag3: '都市',   //标签3
             reviews: 413,  //评论人数
-            value: 3,  //星级(开始为综合星级，评论后为评级的星级)
+            rating: 3,  //星级(开始为综合星级，评论后为评级的星级)
             images: 'https://cdn.vuetifyjs.com/images/cards/cooking.png', //图片位置信息
             title: '神TM斗破', //标题（书名作者名）
-            author: 'Tom Simth',//作者名
+            author: 'Tom Smith',//作者名
             category: '玄幻武侠',//分类信息（可以为数组逐条显示）
-            isfavourite: false, //是否为收藏的书籍、作家
+            isFavourite: false, //是否为收藏的书籍、作家
             messages: 'This is test messages', //简介信息
             flex: 4
           },
